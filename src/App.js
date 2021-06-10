1779d8e86d80f298e5b98becf051f8d6789e32f3
lelienhuong <huonglien212@gmail.com>
2021-06-10 14:28:40 +0700

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HELLO HI
+    </div>
+  );
+}
+
+export default App;
